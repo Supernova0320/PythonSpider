20199378b683b0b0eb409acb85bf30822103f6db
Yucheng Cai <99876883+Supernova0320@users.noreply.github.com>
2023-09-08 18:56:25 +0800

    正则解析2.0

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 import bs4
 
 # 拿到源代码
-start = "0"
+start = 0
 url = f"https://movie.douban.com/top250?start={start}"
 headers = {
     "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36 Edg/116.0.1938.69"
@@ -14,12 +14,14 @@ content = res.text
 # 解析数据1：正则表达式
 obj = re.compile(r'<li>.*?<span class="title">(?P<film_name>.*?)</span>.*?' # 利用惰性匹配保证匹配到第一个span标签（只有第一个才是中文电影名）
                  r'导演: (?P<film_director>.*?)\s.*?'
-                 r'<br>(?P<film_year>.*?)&nbsp', re.S)  # re.S让.可以匹配换行符
+                 r'<br>(?P<film_year>.*?)&nbsp.*?'
+                 r'<span class="rating_num" property="v:average">(?P<film_score>.*?)</span>', re.S)  # re.S让.可以匹配换行符
 result = obj.finditer(content)  # 把content传给既定的表达式，返回一个包含所有匹配结果的可迭代对象
 for i in result:
     print(i.group("film_name"))
     print(i.group("film_director"))
     print(i.group("film_year").strip())
+    print(i.group("film_score"))
 
 # 解析数据2：BeautifulSoup
 
