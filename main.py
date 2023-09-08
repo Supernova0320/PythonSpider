30162a987723ddf992f107d8bd3ef57b789f1d7f
Yucheng Cai <99876883+Supernova0320@users.noreply.github.com>
2023-09-08 18:40:19 +0800

    正则解析

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ res = requests.get(url, headers=headers)
 content = res.text
 
 # 解析数据1：正则表达式
-obj = re.compile(r'<li>.*?<span class="title">(?P<film_name>.*?)</span>'  # 利用惰性匹配确保匹配到第一个span标签（即中文电影名）
-                 r'导演: (P<film_director.*?)\s', re.S)
+obj = re.compile(r'<li>.*?<span class="title">(?P<film_name>.*?)</span>.*?' # 利用惰性匹配保证匹配到第一个span标签（只有第一个才是中文电影名）
+                 r'导演: (?P<film_director>.*?)\s.*?'
+                 r'<br>(?P<film_year>.*?)&nbsp', re.S)  # re.S让.可以匹配换行符
 result = obj.finditer(content)  # 把content传给既定的表达式，返回一个包含所有匹配结果的可迭代对象
 for i in result:
     print(i.group("film_name"))
     print(i.group("film_director"))
+    print(i.group("film_year").strip())
+
 # 解析数据2：BeautifulSoup
 
 res.close()
