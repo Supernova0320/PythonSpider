4b53c335001d074fff1e6b50d7b0ef0ae4bfeaf2
Yucheng Cai <99876883+Supernova0320@users.noreply.github.com>
2023-09-08 18:01:52 +0800

    正则解析

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import re
+import bs4
+
+# 拿到源代码
+start = "0"
+url = f"https://movie.douban.com/top250?start={start}"
+headers = {
+    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36 Edg/116.0.1938.69"
+}
+res = requests.get(url, headers=headers)
+content = res.text
+
+# 解析数据1：正则表达式
+obj = re.compile(r'<li>.*?<span class="title">(?P<film_name>.*?)</span>'  # 利用惰性匹配确保匹配到第一个span标签（即中文电影名）
+                 r'导演: (P<film_director.*?)\s', re.S)
+result = obj.finditer(content)  # 把content传给既定的表达式，返回一个包含所有匹配结果的可迭代对象
+for i in result:
+    print(i.group("film_name"))
+    print(i.group("film_director"))
+# 解析数据2：BeautifulSoup
+
+res.close()
